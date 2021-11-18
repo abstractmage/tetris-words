d8a5089d71bbb3f3e19980c62cb098ab790cdbff
Anna Gekht <annhotmail@inbox.ru>
2021-11-18 16:27:04 +0300

    done (#6)

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,22 +1,30 @@
-import { useLocalObservable } from 'mobx-react';
-import { AppStore } from './store';
-import { AppContext } from './store/context';
-import { Main } from './components/Main';
-import { GamePage } from './components/GamePage';
-// import { StartPage } from './components/StartPage';
-import './index.scss';
-import './shared/styles/index.scss';
+import { observer, useLocalObservable } from "mobx-react";
+import { AppStore } from "./store";
+import { AppContext } from "./store/context";
+import { GamePage } from "./components/GamePage";
+import { StartPage } from "./components/StartPage";
+import "./index.scss";
+import "./shared/styles/index.scss";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import { Main } from "./components/Main";
 
 const { Provider } = AppContext;
 
-export function App() {
+export const App = observer(function App() {
   const appStore = useLocalObservable(() => new AppStore());
   return (
     <Provider value={appStore}>
-      <Main>
-        {/* <StartPage /> */}
-        <GamePage />
-      </Main>
+      <BrowserRouter basename={"/tetris-words"}>
+        <Main>
+          <Routes>
+            <Route path="/" element={<StartPage />} />
+            {appStore.isVisibleGamePage && (
+              <Route path="/game" element={<GamePage />} />
+            )}
+            <Route path="/game" element={<Navigate to="/" />} />
+          </Routes>
+        </Main>
+      </BrowserRouter>
     </Provider>
   );
-};
+});
